059ba51ded35591cda7856eb3c4085999aaf7a28
Sarvesh Dakhane <sarvesh.dakhane@hotmail.com>
2021-05-02 17:25:03 +0530

    Add project files.

---
+++ BookAPI/Models/Books.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPI.Models
+{
+    public class Books
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+
+    }
+
+}
